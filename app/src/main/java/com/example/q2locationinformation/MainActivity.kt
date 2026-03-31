27cbbbadb7cbe2d6ade2046015270825e7a4e41e
annalapradeBU <alaprade@bu.edu>
2026-03-31 13:03:14 -0400

    more comments

--- app/src/main/java/com/example/q2locationinformation/MainActivity.kt
+++ app/src/main/java/com/example/q2locationinformation/MainActivity.kt
@@ -3,17 +3,12 @@ package com.example.q2locationinformation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.q2locationinformation.ui.theme.Q2LocationInformationTheme
 import android.Manifest
-import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Geocoder
 import androidx.activity.compose.rememberLauncherForActivityResult
